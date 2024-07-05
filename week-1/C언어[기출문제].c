5bdf581d40e6d2d0b5579702d3f6cf2910bffa13
Kim Do Young <difbfl4750@gmail.com>
2024-07-06 00:22:02 +0900

    정처기 공부

--- week-1/C언어[기출문제].c
+++ week-1/C언어[기출문제].c
@@ -18,28 +18,55 @@
 //     }
 // }
 
+// void main(){
+//     int i=0, c=0;
+//     while (i<10){
+//         i++;
+//         c *= i;
+//     }
+//     printf("%d", c);
+// }
+
+// int r1(){
+//     return 4;
+// }
+
+// int r10(){
+//     return (30+r1());
+// }
+
+// int r100(){
+//     return (200+r10());
+// }
+
+// int main(){
+//     printf("%d\n", r100());
+//     return 0;
+// }
+
+// void main() {
+//     int a[4] = {0, 2, 4, 8};
+//     int b[3];
+//     int* pl;
+//     int i, j;
+//     int sum = 0;
+//     for (i = 1; i < 4; i++){
+//         pl = a + i;
+//         b[i-1] = *pl - a[i-1];
+//         sum = sum + a[i] + b[i-1];
+//     }
+//     printf("%d", sum);
+// }
+
 void main(){
-    int i=0, c=0;
-    while (i<10){
-        i++;
-        c *= i;
-    }
-    printf("%d", c);
+    char *p = "KOREA";
+    printf("%s\n", p);
+    printf("%s\n", p+3);
+    printf("%c\n", *p);
+    printf("%c\n", *(p+3));
+    printf("%c\n", *p+2);
+    
 }
 
-int r1(){
-    return 4;
-}
 
-int r10(){
-    return (30+r1());
-}
 
-int r100(){
-    return (200+r10());
-}
-
-int main(){
-    printf("%d\n", r100());
-    return 0;
-}
