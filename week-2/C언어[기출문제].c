23864108276bb553502b3d19874ee4ba658c323c
Kim Do Young <difbfl4750@gmail.com>
2024-07-10 17:12:46 +0900

    정처기 공부

--- week-2/C언어[기출문제].c
+++ week-2/C언어[기출문제].c
@@ -93,3 +93,49 @@
 //     }
 //     printf("%d", s);
 // }
+
+// int Soojebi(int base, int exp){
+//     int i, result = 1;
+//     for (i = 0; i < exp; i++)
+//         result *= base;
+//     return result;    
+// }
+
+// void main(){
+//    printf("%d", Soojebi(2, 10));
+// }
+
+// void main(){
+//     int *arr[3];
+//     int a = 12, b = 24, c = 36;
+//     arr[0] = &a;
+//     arr[1] = &b;
+//     arr[2] = &c;
+//     printf("%d\n", *arr[1] + **arr + 1);
+// }
+
+// struct Soojebi{
+//     char name[20];
+//     int os, db, hab1, hab2;
+// };
+
+// void main(){
+//     struct Soojebi s[3] = {{"데이터1", 95, 88}, {"데이터2", 84, 91}, {"데이터3", 86, 75}};
+//     struct Soojebi *p;
+//     p = &s[0];
+//     (p+1)->hab1 = (p+1)->os + (p+2)->db;
+//     (p+1)->hab2 = (p+1)->hab1 + p->os + p->db;
+//     printf("%d\n", (p+1)->hab1 + (p+1)->hab2);    
+// }
+
+// int fn(int a){
+//     if (a <= 1) return 1;
+//     return a * fn(a-1);
+// }
+
+// void main(){
+//     int a;
+//     scanf("%d", &a);
+//     printf("%d", fn(a));
+// }
+
