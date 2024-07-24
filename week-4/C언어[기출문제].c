d62a397f2418f2a5a4dd7aed84afbb5e3bc67d76
Kim Do Young <difbfl4750@gmail.com>
2024-07-24 23:37:25 +0900

    정처기 공부

--- week-4/C언어[기출문제].c
+++ week-4/C언어[기출문제].c
@@ -622,21 +622,32 @@
 //     printf("%d", i);
 // }
 
-void main() {
-    int i, j;
-    int temp;
-    int a[5] = {75, 95, 85, 100, 50};
-    for (i = 0; i < 4; i++){
-        for (j = 0; j < 4-i; j++){
-            if(a[i] > a[j+1]){
-                temp = a[i];
-                a[i] = a[j+1];
-                a[j+1] = temp;
-            }
-        }
-    }
+// void main() {
+//     int i, j;
+//     int temp;
+//     int a[5] = {75, 95, 85, 100, 50};
+//     for (i = 0; i < 4; i++){
+//         for (j = 0; j < 4-i; j++){
+//             if(a[i] > a[j+1]){
+//                 temp = a[i];
+//                 a[i] = a[j+1];
+//                 a[j+1] = temp;
+//             }
+//         }
+//     }
 
-    for(i = 0; i < 5; i++){
-        printf("%d ", a[i]);
-    }
- }
+//     for(i = 0; i < 5; i++){
+//         printf("%d ", a[i]);
+//     }
+//  }
+
+// int fn(int a){
+//     if (a <= 1) return 1;
+//     return a * fn(a-1);
+// }
+
+// void main(){
+//     int a;
+//     scanf("%d", &a);
+//     printf("%d", fn(a));
+// }
