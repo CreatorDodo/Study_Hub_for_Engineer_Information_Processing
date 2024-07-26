c7e7d56a3a5e201be1548bfa766baeb15285b153
Kim Do Young <difbfl4750@gmail.com>
2024-07-26 20:03:43 +0900

    정처기 공부

--- week-4/Soojebi.java
+++ week-4/Soojebi.java
@@ -586,15 +586,15 @@
 //     }
 // }
 
-public class Soojebi{
-    public static void main(String[] args) {
-        int i = 0, j = 0;
-        for(int k = 0; k < 3; k++){
-            if((++i > 1) && (++j > 1)){
-                i++;
-            }
-        }
+// public class Soojebi{
+//     public static void main(String[] args) {
+//         int i = 0, j = 0;
+//         for(int k = 0; k < 3; k++){
+//             if((++i > 1) && (++j > 1)){
+//                 i++;
+//             }
+//         }
 
-        System.out.println(i + " " + j);
-    }
-}
+//         System.out.println(i + " " + j);
+//     }
+// }
